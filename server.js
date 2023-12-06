f31e7dc5e391c77f70bbefc10b1763ef06b16955
binitapatel2 <patelp.binita@gmail.com>
2023-12-05 22:32:51 -0500

    changes made

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 const express = require('express');
 const app = express();
-const path = require('path');
+const port = process.env.PORT || 3000;
 
-const port = process.env.PORT || 5000;
-
-app.use(express.static('public')); // Assuming your static files are in the 'public' directory
-
-// Route to serve your HTML file
-app.get('public/index.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html')); // Adjust the path as needed
-});
+app.use(express.static('public'));
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
